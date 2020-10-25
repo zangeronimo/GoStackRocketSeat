404fb143072abb3bb6a7e64ae7fc63dde307bf43
Luciano Zangeronimo <zangeronimo@gmail.com>
2020-10-25 14:17:17 -0300

    ES6 classes - Exercise 04

---
+++ Bonus/ES6/exercise04.js
@@ -0,0 +1,18 @@
+const empresa = {
+    nome: 'Rocketseat',
+    endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC',
+    }
+};
+
+const { nome, endereco: { cidade, estado } } = empresa;
+   
+console.log(nome);
+console.log(cidade);
+console.log(estado);
+
+function mostraInfo({ nome, idade }) {
+    return `${nome} tem ${idade} anos.`;
+}
+console.log(mostraInfo({ nome: 'Diego', idade: 23 }));
