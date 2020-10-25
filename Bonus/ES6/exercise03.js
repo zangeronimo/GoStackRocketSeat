bbd6a968614ab844fb4b62c2a294b96a3f9d970c
Luciano Zangeronimo <zangeronimo@gmail.com>
2020-10-25 14:09:19 -0300

    ES6 classes - Exercise 03

---
+++ Bonus/ES6/exercise03.js
@@ -0,0 +1,21 @@
+// 3.1
+const arr = [1, 2, 3, 4, 5];
+arr.map(item => item + 10);
+
+// 3.2
+const usuario = { nome: 'Diego', idade: 23 };
+const mostraIdade = usuario => usuario.idade;
+console.log(mostraIdade(usuario));
+
+// 3.3
+const nome = "Diego";
+const idade = 23;
+const mostraUsuario = (nome = 'Diego', idade = 18) =>({ nome, idade });
+console.log(mostraUsuario(nome, idade));
+console.log(mostraUsuario(nome));
+
+// 3.4
+const promise = () => {
+    return new Promise((resolve, reject) => resolve());
+}
+   
