0eb48b46f62962dd86f14594b318f847ff07e717
Luciano Zangeronimo <zangeronimo@gmail.com>
2020-11-10 18:10:12 -0300

    .

---
+++ GoStack/NodeJS/primeiro-projeto-node/src/database/migrations/1604491450382-CreateUsers.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateUsers1604491450382 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
---
+++ GoStack/ReactJS/primeiro-projeto-react/src/pages/Dashboard/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+    font-size: 48px;
+    color: #3A3A3A;
+    max-width: 450px;
+    line-height: 56px;
+
+    margin-top: 80px;
+`;
