d6a93526cc82cd44f12a88775446209d9a9cb2fd
Luciano Zangeronimo <zangeronimo@gmail.com>
2020-12-20 11:22:17 -0300

    celebrate routes validation

--- GoStack/NodeJS/primeiro-projeto-node/src/modules/users/infra/http/routes/password.routes.ts
+++ GoStack/NodeJS/primeiro-projeto-node/src/modules/users/infra/http/routes/password.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
 import ResetPasswordController from '../controllers/ResetPasswordController';
 
@@ -6,7 +8,25 @@ const passwordRoute = Router();
 const forgotPasswordController = new ForgotPasswordController();
 const resetPasswordController = new ResetPasswordController();
 
-passwordRoute.post('/forgot', forgotPasswordController.create);
-passwordRoute.post('/reset', resetPasswordController.create);
+passwordRoute.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create,
+);
+passwordRoute.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  resetPasswordController.create,
+);
 
 export default passwordRoute;
