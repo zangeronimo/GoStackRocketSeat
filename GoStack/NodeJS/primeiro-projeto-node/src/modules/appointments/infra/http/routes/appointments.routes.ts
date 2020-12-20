1235fd6481f9b3ed254ec7ae08ab07ace17caff1
Luciano Zangeronimo <zangeronimo@gmail.com>
2020-12-20 10:37:08 -0300

    Create Notifications Service

--- GoStack/NodeJS/primeiro-projeto-node/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ GoStack/NodeJS/primeiro-projeto-node/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
--- GoStack/NodeJS/primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentService.ts
+++ GoStack/NodeJS/primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,8 +1,9 @@
-import { getHours, isBefore, startOfHour } from 'date-fns';
+import { getHours, isBefore, startOfHour, format } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import AppError from '@shared/errors/AppError';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -16,6 +17,9 @@ class CreateAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository,
   ) {}
 
   public async execute({
@@ -51,6 +55,13 @@ class CreateAppointmentService {
       provider_id,
       user_id,
       date: appointmentDate,
+    });
+
+    const dateFormated = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'");
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento para ${dateFormated}`,
     });
 
     return appointment;
