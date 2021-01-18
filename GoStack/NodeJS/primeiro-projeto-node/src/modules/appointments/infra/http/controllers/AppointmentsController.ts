7e49bd803881613229e095c130bf12c11f360410
Luciano Zangeronimo <zangeronimo@gmail.com>
2021-01-18 17:21:51 -0300

    added cache with Redis

--- GoStack/NodeJS/primeiro-projeto-node/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ GoStack/NodeJS/primeiro-projeto-node/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,6 +1,5 @@
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 import { container } from 'tsyringe';
-import { parseISO } from 'date-fns';
 import { Request, Response } from 'express';
 
 export default class AppointmentsController {
@@ -8,13 +7,11 @@ export default class AppointmentsController {
     const user_id = request.user.id;
     const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
-
     const createAppointment = container.resolve(CreateAppointmentService);
     const appointment = await createAppointment.execute({
       provider_id,
       user_id,
-      date: parsedDate,
+      date,
     });
 
     return response.status(201).json(appointment);
